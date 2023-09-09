b6296f633b96713cbd0d4550bed7e5e22a55103a
TitansProductions <84135181+TitansProductions@users.noreply.github.com>
2023-09-09 14:46:15 +0300

    Update fxmanifest.lua

--- tp_libs/fxmanifest.lua
+++ tp_libs/fxmanifest.lua
@@ -4,7 +4,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 
 author 'Nosmakos'
 description 'Titans Productions Libs'
-version '1.0.0'
+version '1.0.1'
 
 shared_scripts { 'config.lua', 'locales.lua'   }
 client_scripts { 'client/*.lua' }
