2473cb3eb06006cf9a2be9fdd357f7ffdd6b97bf
TitansProductions <84135181+TitansProductions@users.noreply.github.com>
2024-02-08 12:05:35 +0200

    Update tp-client_main.lua

--- tp_libs/client/tp-client_main.lua
+++ tp_libs/client/tp-client_main.lua
@@ -23,3 +23,14 @@ end)
 RegisterNetEvent("gum:SelectedCharacter", function(charId)
     TriggerEvent("tp_libs:isPlayerReady")
 end)
+
+-- RSG CORE
+AddEventHandler('RSGCore:Client:OnPlayerLoaded', function()
+    TriggerEvent("tp_libs:isPlayerReady")
+end)
+
+-- QBCORE
+AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
+    TriggerEvent("tp_libs:isPlayerReady")
+end)
+
