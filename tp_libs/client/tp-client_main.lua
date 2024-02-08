05404142caf5ad3c0041eb545b52603d552acf12
TitansProductions <84135181+TitansProductions@users.noreply.github.com>
2024-02-08 11:57:07 +0200

    Update tp-client_main.lua

--- tp_libs/client/tp-client_main.lua
+++ tp_libs/client/tp-client_main.lua
@@ -1,1 +1,13 @@
 
+-- Triggered when character has been selected from
+-- any framework. 
+RegisterNetEvent("tp_libs:isPlayerReady")
+AddEventHandler("tp_libs:isPlayerReady", function()
+
+end)
+
+-- Gets the player job when character is selected.
+
+AddEventHandler("tpz_core:isPlayerReady", function()
+    TriggerEvent("tp_libs:isPlayerReady")
+end)
