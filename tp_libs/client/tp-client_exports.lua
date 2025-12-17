c582cc94b5c03b71ce13133bfbde932c2d772c1d
TitansProductions <84135181+TitansProductions@users.noreply.github.com>
2025-12-18 00:37:08 +0200

    Delete tp_libs directory

--- tp_libs/client/tp-client_exports.lua
+++
@@ -1,8 +0,0 @@
-
------------------------------------------------------------
---[[ Exports  ]]--
------------------------------------------------------------
-
-exports('RegisterOption', function(resource, title) RegisterOption(resource, title) end)
-
-exports('RemoveOptionRegistration', function(resource) RemoveOptionRegistration(resource) end)
