59202fcde01b0b12c5249c42bbbf6931271876fc
TitansProductions <84135181+TitansProductions@users.noreply.github.com>
2026-09-16 11:45:55 +0300

    Update webhooks.lua

--- tp_libs/server/webhooks.lua
+++ tp_libs/server/webhooks.lua
@@ -14,189 +14,189 @@
 local WEBHOOKS = {
 
    ["tp_companies"] = {
-        ['COMMANDS']  = "",
+        ['COMMANDS']                = "webhook_url_here",
    }, 
       
    ["tp_wildlife_missions"] = {
-        ['RECEIVED_REWARDS']  = "",
+        ['RECEIVED_REWARDS']        = "webhook_url_here",
    }, 
       
-   ["tp_cod_zombies"] = {
-        ['VALENTINE']     = "", -- default map name
+   ["tp_cod_zombies"]               = {
+        ['VALENTINE']               = "webhook_url_here", -- default map name
    }, 
    
     ["tp_stables"] = {
-        ['COMMANDS'] = "",
-        ['BOUGHT'] = "",
-        ['SOLD'] = "",
-        ['SOLD_TAMED_HORSE'] = "",
-        ['TRANSFERRED'] = "",
-        ['RECEIVED_TAMED_HORSE'] = "",
-        ['BREEDING'] = "",
+        ['COMMANDS']                = "webhook_url_here",
+        ['BOUGHT']                  = "webhook_url_here",
+        ['SOLD']                    = "webhook_url_here",
+        ['SOLD_TAMED_HORSE']        = "webhook_url_here",
+        ['TRANSFERRED']             = "webhook_url_here",
+        ['RECEIVED_TAMED_HORSE']    = "webhook_url_here",
+        ['BREEDING']                = "webhook_url_here",
     },
 
       
    ["tp_clans"] = {
-        ['COMMANDS']                = "",
-        ['FAILED_TO_PAY_TAX']       = "",
-        ['DELETED_CLAN']            = "",
-        ['RAIDS']                   = "",
+        ['COMMANDS']                = "webhook_url_here",
+        ['FAILED_TO_PAY_TAX']       = "webhook_url_here",
+        ['DELETED_CLAN']            = "webhook_url_here",
+        ['RAIDS']                   = "webhook_url_here",
     },
       
    ["tp_ranch"] = {
-        ['BOUGHT']                  = "",
-        ['STORES']                  = "",
-        ['LEDGER_TRANSACTIONS']     = "",
-        ['MEMBERS']                 = "",
-        ['CHEAT']                   = "",
-        ['FAILED_TO_PAY_TAX']       = "",
+        ['BOUGHT']                  = "webhook_url_here",
+        ['STORES']                  = "webhook_url_here",
+        ['LEDGER_TRANSACTIONS']     = "webhook_url_here",
+        ['MEMBERS']                 = "webhook_url_here",
+        ['CHEAT']                   = "webhook_url_here",
+        ['FAILED_TO_PAY_TAX']       = "webhook_url_here",
    },
       
    ["tp_trash"] = {
-        ['ALL']                     = "",
+        ['ALL']                     = "webhook_url_here",
     },
       
     ["tp_backpacks"] = {
-        ['ALL']                     = "",
+        ['ALL']                     = "webhook_url_here",
     },
 
     ["tp_bandits"] = {
-        ['SUCCESS_LOOT']            = "",
-        ['ROBBED_BY_BANDITS']       = "",
-        ['CHEAT']                   = "",
+        ['SUCCESS_LOOT']            = "webhook_url_here",
+        ['ROBBED_BY_BANDITS']       = "webhook_url_here",
+        ['CHEAT']                   = "webhook_url_here",
     },
 
     ["tp_banks"] = {
-        ['TRANSACTIONS']            = "",
-        ['ROBBERIES']               = "",
+        ['TRANSACTIONS']            = "webhook_url_here",
+        ['ROBBERIES']               = "webhook_url_here",
     },
 
     ["tp_beekeepers"] = {
-        ['ALL']                     = "",
+        ['ALL']                     = "webhook_url_here",
     },
 
     ["tp_crayfish"] = {
-        ['CRAYFISH_CATCH_SUCCESS']  = "",
-        ['CHEAT']                   = "",
+        ['CRAYFISH_CATCH_SUCCESS']  = "webhook_url_here",
+        ['CHEAT']                   = "webhook_url_here",
     },
 
     ["tp_grave_robberies"] = {
-        ['ALL']                     = "",
+        ['ALL']                     = "webhook_url_here",
     },
 
     ["tp_housing"] = {
-        ['BOUGHT']                  = "",
-        ['SOLD']                    = "",
-        ['TRANSFERRED']             = "",
-        ['UPGRADE']                 = "",
+        ['BOUGHT']                  = "webhook_url_here",
+        ['SOLD']                    = "webhook_url_here",
+        ['TRANSFERRED']             = "webhook_url_here",
+        ['UPGRADE']                 = "webhook_url_here",
     },
 
     ["tp_legendhunting"] = {
-        ['STARTED']                 = "",
-        ['SUCCESS']                 = "",
-        ['CHEAT']                   = "",
+        ['STARTED']                 = "webhook_url_here",
+        ['SUCCESS']                 = "webhook_url_here",
+        ['CHEAT']                   = "webhook_url_here",
     },
 
     ["tp_medical_archives"] = {
 
         -- MEDICAL_ IS THE DEPARTMENT FROM CONFIG
         -- IN CASE YOU CREATE MORE DEPARTMENTS, YOU MUST ALSO ADD THEM BELOW.
-        ['MEDICAL_REGISTER']                = "",
-        ['MEDICAL_NEW_FORM']                = "",
-        ['MEDICAL_PATIENT_INFO']            = "",
-        ['MEDICAL_DELETED_FORM']            = "",
-        ['MEDICAL_DELETED_PATIENT']         = "",
+        ['MEDICAL_REGISTER']        = "webhook_url_here",
+        ['MEDICAL_NEW_FORM']        = "webhook_url_here",
+        ['MEDICAL_PATIENT_INFO']    = "webhook_url_here",
+        ['MEDICAL_DELETED_FORM']    = "webhook_url_here",
+        ['MEDICAL_DELETED_PATIENT'] = "webhook_url_here",
     },
 
     ["tp_mydog"] = {
-        ['BUY']                     = "",
-        ['SELL']                    = "",
-        ['TRANSFER']                = "",
+        ['BUY']                     = "webhook_url_here",
+        ['SELL']                    = "webhook_url_here",
+        ['TRANSFER']                = "webhook_url_here",
     },
 
     ["tp_oilfields"] = {
-        ['WORKER_ACTIONS']          = "",
-        ['DELIVERY_ACTIONS']        = "",
-        ['MANAGEMENT_ACTIONS']      = "",
-        ['CHEAT']                   = "",
+        ['WORKER_ACTIONS']          = "webhook_url_here",
+        ['DELIVERY_ACTIONS']        = "webhook_url_here",
+        ['MANAGEMENT_ACTIONS']      = "webhook_url_here",
+        ['CHEAT']                   = "webhook_url_here",
     },
 
     ["tp_pinboard"] = {
-        ['POSTED_NOTE']             = "",
-        ['DELETED_POSTED_NOTE']     = "",
-        ['BLACKLISTED_URL']         = "",
+        ['POSTED_NOTE']             = "webhook_url_here",
+        ['DELETED_POSTED_NOTE']     = "webhook_url_here",
+        ['BLACKLISTED_URL']         = "webhook_url_here",
 
         -- Below, for TP Pinboards you will need to register from Config.Locations (if you want active webhooks)
         -- the webhooks by the location name (Default config below as an example).
 
-        ['VALENTINE']               = "",
-        ['VALENTINE_MAILBOX']       = "",
-        ['RHODES']                  = "",
-        ['STRAWBERRY']              = "",
-        ['BLACKWATER']              = "",
-        ['SAINT_DENIS']             = "",
+        ['VALENTINE']               = "webhook_url_here",
+        ['VALENTINE_MAILBOX']       = "webhook_url_here",
+        ['RHODES']                  = "webhook_url_here",
+        ['STRAWBERRY']              = "webhook_url_here",
+        ['BLACKWATER']              = "webhook_url_here",
+        ['SAINT_DENIS']             = "webhook_url_here",
 
     },
 
     ["tp_player_auctions"] = {
-        ['ALL']                     = "",
-        ['CREATE']                  = "",
-        ['DELETE']                  = "",
+        ['ALL']                     = "webhook_url_here",
+        ['CREATE']                  = "webhook_url_here",
+        ['DELETE']                  = "webhook_url_here",
     },
     
     ["tp_stores"] = {
-        ['BOUGHT']                  = "",
-        ['SOLD']                    = "",
-        ['EXCHANGED']               = "",
+        ['BOUGHT']                  = "webhook_url_here",
+        ['SOLD']                    = "webhook_url_here",
+        ['EXCHANGED']               = "webhook_url_here",
     },
 
     ["tp_player_stores"] = {
-        ['CREATE']                  = "",
-        ['DELETE']                  = "",
-        ['RENTED']                  = "",
-        ['CANCELLED_RENTING']       = "",
-        ['WITHDREW_ACCOUNT']        = "",
-        ['DEPOSITED_ACCOUNT']       = "",
-        ['BOUGHT']                  = "",
-        ['ADD_PRODUCTS']            = "",
-        ['REMOVE_PRODUCTS']         = "",
-        ['FAILED_TO_PAY_TAX']       = "",
-        ['SOLD_PRODUCTS']           = "",
-        ['BOUGHT_PRODUCTS']         = "",
+        ['CREATE']                  = "webhook_url_here",
+        ['DELETE']                  = "webhook_url_here",
+        ['RENTED']                  = "webhook_url_here",
+        ['CANCELLED_RENTING']       = "webhook_url_here",
+        ['WITHDREW_ACCOUNT']        = "webhook_url_here",
+        ['DEPOSITED_ACCOUNT']       = "webhook_url_here",
+        ['BOUGHT']                  = "webhook_url_here",
+        ['ADD_PRODUCTS']            = "webhook_url_here",
+        ['REMOVE_PRODUCTS']         = "webhook_url_here",
+        ['FAILED_TO_PAY_TAX']       = "webhook_url_here",
+        ['SOLD_PRODUCTS']           = "webhook_url_here",
+        ['BOUGHT_PRODUCTS']         = "webhook_url_here",
 
     },
 
     ["tp_stagecoach"] = {
-        ['BOUGHT_TRANSPORT_VEHICLE']  = "",
-        ['CLIENT_DELIVERY_REWARDS']   = "",
+        ['BOUGHT_TRANSPORT_VEHICLE'] = "webhook_url_here",
+        ['CLIENT_DELIVERY_REWARDS']  = "webhook_url_here",
     },
 
     ["tp_trading_port"] = {
-        ['BOUGHT_STORAGE']           = "",
-        ['SUBMITTED_NEW_ORDER']      = "",
-        ['SOLD_PRODUCT']             = "",
+        ['BOUGHT_STORAGE']           = "webhook_url_here",
+        ['SUBMITTED_NEW_ORDER']      = "webhook_url_here",
+        ['SOLD_PRODUCT']             = "webhook_url_here",
     },
 
     ["tp_warehouses"] = {
-        ['BOUGHT']                   = "",
-        ['LOST_OWNERSHIP']           = "",
+        ['BOUGHT']                   = "webhook_url_here",
+        ['LOST_OWNERSHIP']           = "webhook_url_here",
     },
 
     ["tp_warehouses"] = {
-        ['BOUGHT']                   = "",
-        ['LOST_OWNERSHIP']           = "",
+        ['BOUGHT']                   = "webhook_url_here",
+        ['LOST_OWNERSHIP']           = "webhook_url_here",
     },
 
     ["tp_world_looting"] = {
-        ['ALL']                      = "",
+        ['ALL']                      = "webhook_url_here",
     },
 
     ["tp_mailbox"] = {
 
-        ['REGISTERED']              = "",
-        ['SENT_TELEGRAM']           = "",
-        ['SENT_PARCEL']             = "",
-        ['RETURNED_PARCEL']         = "",
+        ['REGISTERED']              = "webhook_url_here",
+        ['SENT_TELEGRAM']           = "webhook_url_here",
+        ['SENT_PARCEL']             = "webhook_url_here",
+        ['RETURNED_PARCEL']         = "webhook_url_here",
 
     },
     
